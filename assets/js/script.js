1c5a7d27f3d42a90de23745f885b1dd11a355937
moon9588 <moon9588@yahoo.com>
2022-01-07 19:23:42 -0500

    worked on java

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let userFormE1 = document.querySelector("#user-form");
+let nameInputE1 = document.querySelector("#city");
+let tempE1 = document.querySelector("#temp");
+
+let containerE1 = document.querySelector("#container");
+let cityNameE1 = document.querySelector(".city-name");
+let fiveDayContainer = document.querySelector('#forecast');
+
+let humidityEl = document.querySelector("#container");
+let humidityEl = document.querySelector("#wind");
+
+
+let apiKey= ""
